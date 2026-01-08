ba1b35f487a6c0b3fb72f1d8c3d13ad36328e54e
Mohmmed Elfateh Sabry <mohamed.alfateh2@ai.menofia.edu.eg>
2026-01-08 09:20:51 +0200

    fix: update Hijri filter to use Date.today for consistent date calculation

--- _plugins/hijri_filter.rb
+++ _plugins/hijri_filter.rb
@@ -3,8 +3,7 @@ require 'hijri'
 module Jekyll
   module HijriFilter
     def hijri_date(input)
-      date = input.to_date
-      hijri = date.to_hijri
+      hijri = Date.today.to_hijri
       hijri.strftime('%-d %B %Y AH')
     end
   end
